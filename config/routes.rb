b6718273663b734db7cf247ffe6f393fff76afa3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-01 17:51:45 +0100

    set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # get 'restaurants' => 'restaurants#index'
+  root "restaurants#index"
 
   resources :restaurants do
     resources :reviews
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
