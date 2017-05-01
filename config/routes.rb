017576f3e35c4ca04ae3a48cdaa341f13192ff89
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-01 13:54:03 +0100

    add reviews feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   # get 'restaurants' => 'restaurants#index'
 
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -62,3 +62,32 @@ context 'editing restaurants' do
   end
 
 end
+
+context 'deleting restaurants' do
+
+  before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+  scenario 'removes a restaurant when a user clicks a delete link' do
+    visit '/restaurants'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
+  end
+
+end
+
+feature 'reviewing' do
+  before { Restaurant.create name: 'KFC' }
+
+  scenario 'allows users to leave a review using a form' do
+     visit '/restaurants'
+     click_link 'Review KFC'
+     fill_in "Thoughts", with: "so so"
+     select '3', from: 'Rating'
+     click_button 'Leave Review'
+
+     expect(current_path).to eq '/restaurants'
+     expect(page).to have_content('so so')
+  end
+
+end
